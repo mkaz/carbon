1039e4c672f415d3439ba03d9af11504091d2e5e
Marcus Kazmierczak <marcus@mkaz.com>
2019-11-10 19:26:24 -0800

    More tweaks

---
+++ src/single.js
@@ -0,0 +1,11 @@
+// External
+import React from "react";
+import ReactDOM from "react-dom";
+
+// Internal
+import { Post } from './components';
+
+ReactDOM.render(
+	<Post post={ post }/>,
+	document.getElementById( 'post' )
+);
